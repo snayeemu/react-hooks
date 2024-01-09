1f6af62511af40a6681e2f99e65aaad19a42446e
snayeemu <snayeemu2@gmail.com>
2024-01-09 14:39:17 +0600

    useState example added

--- src/pages/UseStateExample.tsx
+++ src/pages/UseStateExample.tsx
@@ -1,21 +1,41 @@
-type CounterType = {
-  counter: number;
-  setCounter: React.Dispatch<React.SetStateAction<number>>;
-};
+import { useState } from "react";
 
-const UseStateExample = ({ counter, setCounter }: CounterType) => {
-  console.log("render");
+const UseStateExample = () => {
+  // const [name, setName] = useState("");
+  // const [email, setEmail] = useState("");
+
+  const [user, setUser] = useState({ name: "", email: "" });
+
+  const handleSubmit = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    console.log(user);
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setUser((prev) => {
+      return { ...prev, [e.target.name]: e.target.value };
+    });
 
   return (
-    <div>
-      <h1 className="text-2xl">{counter}</h1>
-      <button
-        className="btn bg-blue-500 text-white px-2 py-3"
-        onClick={() => setCounter((prev) => prev + 1)}
-      >
-        Increment
+    <form onSubmit={handleSubmit}>
+      <input
+        className="border-2"
+        onChange={handleChange}
+        type="text"
+        name="name"
+        id="name"
+      />
+      <input
+        className="border-2"
+        onChange={handleChange}
+        type="email"
+        name="email"
+        id="email"
+      />
+      <button className="bg-blue-200" type="submit">
+        Submit
       </button>
-    </div>
+    </form>
   );
 };
 
